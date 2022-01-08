4da4eddf45af1e927d927cb34d59949706df8db2
mslmyrtd <mslmyrtd@gmail.com>
2022-01-08 16:40:56 +0300

    register formik form added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,84 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
-const Navbar = () => {
-  return <div>navbar</div>;
-};
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { useNavigate } from "react-router-dom";
 
-export default Navbar;
+export default function Navbar() {
+  const navigate = useNavigate();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {"<Mslm />"}
+            BLOG
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={() => navigate("/Login")}>Login</MenuItem>
+                <MenuItem onClick={() => navigate("/Register")}>
+                  Register
+                </MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,154 @@
 import React from "react";
+import { LockOutlined } from "@mui/icons-material";
 
-const Register = () => {
-  return <div>register</div>;
-};
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { blue } from "@mui/material/colors";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const signUpValidationSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid Email").required("Email is required"),
+  password: Yup.string()
+    .required("No password provided")
+    .min(8, "Password is too short - should be 8 chars minimum")
+    .matches(/\d+/, "Password must have a number")
+    .matches(/[a-z]+/, "Password must have a lowercase")
+    .matches(/[A-Z]+/, "Password must have a uppercase")
+    .matches(/[!?.@#$%^&*()-+]+/, "Password must have a special char"),
+});
+
+function Register() {
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    // console.log(values);
+    alert(
+      `username: ${values.username}
+      email: ${values.email}
+      password: ${values.password}
+      password2: ${values.password2}`
+    );
+    resetForm();
+  };
+
+  return (
+    <Container
+      sx={{
+        marginTop: "3rem",
+        // mt: 6,
+        height: "calc(100vh - 3rem)",
+        textAlign: "center",
+        border: "2px solid red",
+      }}
+      maxWidth="sm"
+    >
+      <Avatar
+        sx={{
+          margin: "1rem auto",
+          bgcolor: "primary.main",
+          // bgcolor: blue[500],
+        }}
+      >
+        <LockOutlined />
+      </Avatar>
+      <Typography sx={{ margin: "1rem" }} variant="h4">
+        ----REGISTER----
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={signUpValidationSchema}
+      >
+        {({
+          values,
+          handleChange,
+          handleSubmit,
+          touched,
+          errors,
+          handleBlur,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  helperText={touched.email && errors.email}
+                  error={touched.email && Boolean(errors.email)}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  label="Password"
+                  type="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  helperText={touched.password && errors.password}
+                  error={touched.password && Boolean(errors.password)}
+                  fullWidth
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Register
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Google
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      </Formik>
+      <p>
+        Already have an account?
+        <Link
+          sx={{
+            textDecoration: "none",
+            fontWeight: "600",
+            paddingLeft: "0.5rem",
+          }}
+          href="/login"
+        >
+          Login.
+        </Link>
+      </p>
+    </Container>
+  );
+}
 
 export default Register;
