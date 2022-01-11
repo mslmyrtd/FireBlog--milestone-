9779b4741da950464bb923c1b7f77f16d2f2050e
mslmyrtd <mslmyrtd@gmail.com>
2022-01-12 02:14:00 +0300

    style added

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,5 +1,17 @@
 const Details = () => {
-  return <div style={{ marginTop: "5rem" }}>Details</div>;
+  return (
+    <div
+      style={{
+        marginTop: "8rem",
+        fontSize: "3rem",
+        color: "#046582",
+        fontFamily: "Girassol",
+        fontWeight: 800,
+      }}
+    >
+      <h3>─── Details ───</h3>
+    </div>
+  );
 };
 
 export default Details;
